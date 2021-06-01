cc98362070d4d4bcc9732f43f67cd1aea8ba7e87
Amalgamus4 <rc.ward@yahoo.com>
2021-05-31 21:29:13 -0600

    Add menu items and adjust contact page

--- src/contact.js
+++ src/contact.js
@@ -6,9 +6,10 @@ const contactInformation = {
   city: "Anytown",
   state: "USA",
   zip: "11111",
+  email: "anon@anon.com",
 };
 export default function contactTab() {
   const element = document.createElement("p");
-  element.innerHTML = `${contactInformation.tel} <br>  ${contactInformation.address} <br> ${contactInformation.city}, ${contactInformation.state}  ${contactInformation.zip}`;
+  element.innerHTML = `Call us at ${contactInformation.tel} <br> <br> or stop by our downtown location <br> <br> ${contactInformation.address} <br> ${contactInformation.city}, ${contactInformation.state} &nbsp ${contactInformation.zip} <br> <br> You can also drop us a line at <a href="#">${contactInformation.email}</a>`;
   document.getElementById("content").appendChild(element);
 }
--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,26 @@
 // export a function that creates a div element, adds content, and
 // styles that element and then appends to DOM
-const menuItem1 = "Here's item 1";
+const menuItem1 = "Chicken a la Stuff";
+const menuItem1Description =
+  "A delightful dish of chicken, cheese, and gummi bears";
+
+const menuItem2 = "Everything but the Kitchen Sink";
+const menuItem2Description = "Exactly what it sounds like";
+
 import navbar from "./header";
 
 export default function menuTab() {
-  const element = document.createElement("p");
+  const element = document.createElement("h3");
   element.textContent = menuItem1;
+  const element2 = document.createElement("p");
+  element2.textContent = menuItem1Description;
   document.getElementById("content").appendChild(element);
+  document.getElementById("content").appendChild(element2);
+
+  const element3 = document.createElement("h3");
+  element3.textContent = menuItem2;
+  const element4 = document.createElement("p");
+  element4.textContent = menuItem2Description;
+  document.getElementById("content").appendChild(element3);
+  document.getElementById("content").appendChild(element4);
 }
